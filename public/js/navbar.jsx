de3ef09e48f5dd91f56fcfc021d61539e1828d8e
Nick Raienko <enaqxx@gmail.com>
2014-09-06 20:36:07 +0300

    Implement react-bbotstrap components

---
+++ public/js/navbar.jsx
@@ -0,0 +1,36 @@
+/** @jsx React.DOM */
+
+var CommentBox = React.createClass({
+  render: function() {
+    return (
+      <div className="commentBox">
+        Hello, world! I am a CommentBox.
+      </div>
+    );
+  }
+});
+React.renderComponent(
+  <CommentBox />,
+  document.getElementById('nav')
+);
+
+/* var navbarInstance = (
+    <Navbar>
+      <Nav>
+        <NavItem key={1} href="#">Link</NavItem>
+        <NavItem key={2} href="#">Link</NavItem>
+        <DropdownButton key={3} title="Dropdown">
+          <MenuItem key="1">Action</MenuItem>
+          <MenuItem key="2">Another action</MenuItem>
+          <MenuItem key="3">Something else here</MenuItem>
+          <MenuItem divider />
+          <MenuItem key="4">Separated link</MenuItem>
+        </DropdownButton>
+      </Nav>
+    </Navbar>
+  );
+
+React.renderComponent(
+  navbarInstance, 
+  document.getElementById("nav")
+); */
